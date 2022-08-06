86d011cac407b8188621c28260c83b76398f6863
JeremyEllingham <jeremy@sethindustries.com.au>
2022-08-07 00:07:03 +0100

    Scaffold transaction success.

---
+++ src/components/views/utility/TransactionSuccessView/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Image, Text } from "react-native";
+import TYPOGRAPHY from "../../../../design/typography";
+import styles from "./styles";
+
+function TransactionSuccessView() {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require("../../../../assets/images/logo.jpg")}
+      />
+      <Text style={TYPOGRAPHY.h1}>Learn</Text>
+    </View>
+  );
+}
+
+export default TransactionSuccessView;
