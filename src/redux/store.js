d00842edf84fc3388c7e2890f3e486df368c768d
JeremyEllingham <jeremy@sethindustries.com.au>
2022-08-06 16:09:34 +0100

    Add redux logger.

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { applyMiddleware, createStore, compose } from "redux";
 import { persistReducer } from "redux-persist";
 import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import logger from "redux-logger";
 
 import rootReducer from "./rootReducer";
 import initialState from "./initialState";
@@ -17,7 +18,7 @@ const store = createStore(
     rootReducer
   ),
   initialState,
-  compose(applyMiddleware())
+  compose(applyMiddleware(logger))
 );
 
 export default store; // Alow other methods to import store for '.getState()' purposes
