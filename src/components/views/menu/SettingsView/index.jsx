107c348ee952cdc5f1e0d7ddb6a1f6531b21b5dd
JeremyEllingham <jeremy@sethindustries.com.au>
2022-08-07 01:47:06 +0100

    Add settings switch.

--- src/components/views/menu/SettingsView/index.jsx
+++ src/components/views/menu/SettingsView/index.jsx
@@ -1,9 +1,14 @@
-import React from "react";
-import { View, Image, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Image, Text, Switch } from "react-native";
 import TYPOGRAPHY from "../../../../design/typography";
 import styles from "./styles";
+import COLOURS from "../../../../design/colours";
 
 function SettingsView() {
+  const [isEnabled, setIsEnabled] = useState(false);
+
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+
   return (
     <View style={styles.container}>
       <Image
@@ -11,6 +16,15 @@ function SettingsView() {
         source={require("../../../../assets/images/logo.jpg")}
       />
       <Text style={TYPOGRAPHY.h1}>Settings</Text>
+      <View style={styles.container}>
+        <Switch
+          trackColor={{ true: COLOURS.bchGreen, false: COLOURS.white }}
+          thumbColor={isEnabled ? COLOURS.white : COLOURS.black}
+          ios_backgroundColor={COLOURS.lightGrey}
+          onValueChange={toggleSwitch}
+          value={isEnabled}
+        />
+      </View>
     </View>
   );
 }
