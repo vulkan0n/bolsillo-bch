45170f889fa6ee993b2bc312b6ddd19eb18e9ecf
JeremyEllingham <jeremy@sethindustries.com.au>
2022-08-07 12:43:37 +0100

    Update NumPad.

--- src/components/NumPad/index.js
+++ src/components/NumPad/index.js
@@ -7,15 +7,7 @@ import TYPOGRAPHY from "../../design/typography";
 import { displaySat, displayUsd } from "../../utils/formatting";
 import { BRIDGE_MESSAGE_TYPES } from "../../utils/bridgeMessages";
 
-const SATOSHI = 100000000;
-const MAX_SATOSHI = 21000000 * SATOSHI;
-
-function formatSats(sats) {
-  return parseFloat(parseInt(sats) / SATOSHI).toFixed(8);
-}
-
-const NumPad = ({ isCryptoDenominated, navigation }) => {
-  const availableBalance = 32342;
+const NumPad = ({ isCryptoDenominated, balance, navigation }) => {
   const numPadSatBalance = displaySat(0);
   const numPadUsdBalance = displayUsd(0);
 
@@ -31,63 +23,32 @@ const NumPad = ({ isCryptoDenominated, navigation }) => {
     }, 2000);
   }, [inputError]);
 
-  function InputButton({ n }) {
-    const onPress = () => {
-      if (n === "<") {
-        handleInputDelete();
+  const onPress = (n) => {
+    if (n === "<") {
+      if (inputBalance?.length > 1) {
+        setInputBalance(inputBalance?.slice(0, inputBalance?.length - 1));
       } else {
-        handleInput(n);
+        setInputBalance("0");
       }
-    };
+      return;
+    }
+    if (n === "." && inputBalance.includes(".")) {
+      setInputError("Already used decimal point.");
+      return;
+    }
 
-    const onLongPress = () => {
-      if (n === "<") {
-        handleInputClear();
-      }
-    };
-
-    return (
-      <Pressable onPress={onPress} onLongPress={onLongPress}>
-        <Text style={TYPOGRAPHY.h1black}>{n}</Text>
-      </Pressable>
-    );
-  }
-
-  function handleInputDelete() {
-    if (inputBalance?.length > 1) {
-      setInputBalance(inputBalance?.slice(0, inputBalance?.length - 1));
+    if (inputBalance === "0") {
+      setInputBalance(n);
     } else {
+      setInputBalance(inputBalance + n);
+    }
+  };
+
+  const onLongPress = ({ n }) => {
+    if (n === "<") {
       setInputBalance("0");
     }
-  }
-
-  function handleInputClear() {
-    setInputBalance("0");
-  }
-
-  function handleInput(n) {
-    const sats = parseInt(inputBalance + n);
-    if (inputBalance === "0") {
-      if (n === ".") {
-        setInputBalance("0" + n);
-      } else {
-        setInputBalance(n);
-      }
-    } else if (sats <= MAX_SATOSHI && sats <= availableBalance) {
-      if (n === "." && inputBalance.includes(".")) {
-        setInputError("Already used decimal point.");
-        return;
-      }
-
-      if (inputBalance.includes(".") && n !== ".") {
-        const satSplit = inputBalance.split(".");
-        const trySats = satSplit[0] + "." + satSplit[1].substring(0, 7) + n;
-        setInputBalance(trySats);
-      } else {
-        setInputBalance(inputBalance + n);
-      }
-    }
-  }
+  };
 
   const onPressSend = () => {
     const testNetFaucet = "bchtest:qzl7ex0q35q2d6aljhlhzwramp09n06fry8ssqu0qp";
@@ -100,6 +61,14 @@ const NumPad = ({ isCryptoDenominated, navigation }) => {
         satsToSend: "100",
       },
     });
+  };
+
+  const InputButton = ({ n }) => {
+    return (
+      <Pressable onPress={() => onPress(n)} onLongPress={() => onLongPress(n)}>
+        <Text style={TYPOGRAPHY.h1black}>{n}</Text>
+      </Pressable>
+    );
   };
 
   return (
@@ -116,7 +85,6 @@ const NumPad = ({ isCryptoDenominated, navigation }) => {
         {!!inputError && <Text style={styles.inputError}>{inputError}</Text>}
       </View>
       <View style={styles.numPad}>
-        {/* <InputWidget availableBalance={satoshiBalance} /> */}
         <View style={styles.numPadRow}>
           <InputButton n={"1"} />
           <InputButton n={"2"} />
@@ -149,7 +117,8 @@ const NumPad = ({ isCryptoDenominated, navigation }) => {
     </View>
   );
 };
-const mapStateToProps = ({ isCryptoDenominated }) => ({
+const mapStateToProps = ({ balance, isCryptoDenominated }) => ({
+  balance,
   isCryptoDenominated,
 });
 
