f12bff00c67d7cca7531a3e317af0fa96ef1cc2e
JeremyEllingham <jeremy@sethindustries.com.au>
2022-08-10 13:05:31 +0100

    Update reducer.

--- src/redux/reducers/transactionPadReducer.ts
+++ src/redux/reducers/transactionPadReducer.ts
@@ -17,15 +17,12 @@ const transactionPadSlice = createSlice({
   initialState,
   reducers: {
     updateTransactionPadBalance(state, action) {
-      console.log("updating balance", state);
       state.padBalance = action.payload.padBalance;
     },
     updateTransactionPadView(state, action) {
-      console.log("updating view", state);
       state.view = action.payload.view;
     },
     updateTransactionPadError(state, action) {
-      console.log("updating error", state);
       state.error = action.payload.error;
     },
   },
