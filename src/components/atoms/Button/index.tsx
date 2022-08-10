16c53863ba96cc2d6174d9f40000e44bd4504789
JeremyEllingham <jeremy@sethindustries.com.au>
2022-08-10 12:56:55 +0100

    Typescript updates.

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -2,16 +2,23 @@ import React from "react";
 import { Text, TouchableOpacity } from "react-native";
 import styles from "./styles";
 
+interface Props {
+  children: any;
+  variant?: "primary" | "secondary";
+  isSmall?: boolean;
+  onPress: () => void;
+}
+
 const Button = ({
   children,
   variant = "primary",
   isSmall,
   onPress = () => {},
-}) => {
+}: Props) => {
   return (
     <TouchableOpacity
       onPress={onPress}
-      style={styles({ variant, isSmall }).button}
+      style={styles({ variant, isSmall }).button as any}
     >
       <Text style={styles({ variant, isSmall }).buttonText}>{children}</Text>
     </TouchableOpacity>
