35d2514878a6a7e897dae5b1c176d57716722eac
JeremyEllingham <jeremy@sethindustries.com.au>
2022-08-10 11:08:33 +0100

    Update to configureStore.

---
+++ src/redux/store.ts
@@ -0,0 +1,38 @@
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import logger from "redux-logger";
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+import rootReducer from "./rootReducer";
+import initialState from "./initialState";
+
+const persistedReducer = persistReducer(
+  {
+    key: "root",
+    storage: AsyncStorage,
+    stateReconciler: autoMergeLevel2,
+  },
+  rootReducer
+);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export default store; // Alow other methods to import store for '.getState()' purposes
