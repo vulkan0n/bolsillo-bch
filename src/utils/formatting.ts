ab771961d57a0f5e209726ea15900fa91638772b
JeremyEllingham <jeremy@sethindustries.com.au>
2022-08-11 05:00:29 +0100

    Add spaced chunks formatting.

--- src/utils/formatting.ts
+++ src/utils/formatting.ts
@@ -13,12 +13,32 @@ export const displayUsd = (sats: string): string => {
   return `USD $${res.toFixed(2)}`;
 };
 
+// Split string into groups of 3 characters, starting from right side
+// https://stackoverflow.com/a/63716019
+function chunkRight(str, size = 3) {
+  if (typeof str === "string") {
+    const length = str.length;
+    const chunks = Array(Math.ceil(length / size));
+    if (length) {
+      chunks[0] = str.slice(0, length % size || size);
+      for (let i = 1, index = chunks[0].length; index < length; i++) {
+        chunks[i] = str.slice(index, (index += size));
+      }
+    }
+    return chunks;
+  }
+}
+
 export const displaySats = (sats: string): string => {
   if (!sats) {
     return "0 sats";
   }
 
-  return `${parseFloat(sats)} sats`;
+  const floatSats = parseFloat(sats);
+  // i.e. '1 000 120' not `1000120`
+  const spacedChunks = chunkRight(floatSats.toString()).join(" ");
+
+  return `${spacedChunks} sats`;
 };
 
 export const displaySatsAsUsd = (sats: string): string => {
