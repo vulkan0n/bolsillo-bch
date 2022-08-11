ff8e228d28eb3c9fd4a52b17f26f6a5921f03404
JeremyEllingham <jeremy@sethindustries.com.au>
2022-08-11 13:58:26 +0100

    Add youtube link.

--- src/components/views/menu/CommunityView/MusicView/songs.tsx
+++ src/components/views/menu/CommunityView/MusicView/songs.tsx
@@ -3,19 +3,19 @@ const songs = [
     id: 1,
     title: "D1$L0YAL (Music video)",
     artist: "The Bitcoin Cash Podcast",
-    youtubeLink: "https://youtube.com",
+    youtubeLink: "https://www.youtube.com/watch?v=qxsRnsDzz4A",
   },
   {
     id: 2,
     title: "D1$L0YAL (Music video)",
     artist: "Bitcoin Out Loud",
-    youtubeLink: "https://youtube.com",
+    youtubeLink: "https://www.youtube.com/watch?v=qxsRnsDzz4A",
   },
   {
     id: 3,
     title: "D1$L0YAL (Music video)",
     artist: "The Bitcoin Cash Podcast",
-    youtubeLink: "https://youtube.com",
+    youtubeLink: "https://www.youtube.com/watch?v=qxsRnsDzz4A",
   },
 ];
 
