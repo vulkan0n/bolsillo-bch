9dcee4659d62453efebf4455f4f9853ef9cfcc9d
JeremyEllingham <jeremy@sethindustries.com.au>
2022-08-13 18:53:02 +0100

    Update delete wallet.

--- src/components/NavigationTree/ToolsDrawerNavigator/WalletManagerStack/DeleteWalletView/index.tsx
+++ src/components/NavigationTree/ToolsDrawerNavigator/WalletManagerStack/DeleteWalletView/index.tsx
@@ -6,10 +6,16 @@ import styles from "./styles";
 import Toast from "react-native-toast-message";
 import { useSelector } from "react-redux";
 import { ReduxState } from "../../../../../types";
+import Divider from "../../../../atoms/Divider";
 
 const DeleteWalletView = ({ navigation }) => {
   const { navigatedWalletName } = useSelector(
     (state: ReduxState) => state.walletManager
+  );
+  const { name, description } = useSelector((state: ReduxState) =>
+    state.walletManager.wallets?.find(
+      ({ name }) => name === navigatedWalletName
+    )
   );
 
   const onPressBackup = () => {
@@ -32,12 +38,19 @@ const DeleteWalletView = ({ navigation }) => {
 
   return (
     <View style={styles.container as any}>
+      <Text style={TYPOGRAPHY.h2 as any}>{name}</Text>
+      <Text style={TYPOGRAPHY.pWhite as any}>{description}</Text>
+      <Divider />
       <Text style={TYPOGRAPHY.pWhite as any}>
         Note, this will erase all of this wallet's:
       </Text>
       <Text style={TYPOGRAPHY.pWhite as any}>- Wallet data</Text>
       <Text style={TYPOGRAPHY.pWhite as any}>- Settings</Text>
       <Text style={TYPOGRAPHY.pWhite as any}>- Mnemonic phrase</Text>
+      <Text style={TYPOGRAPHY.pWhite as any}>
+        You will lose access to any future coins sent to addresses in this
+        wallet!!
+      </Text>
       <Text style={TYPOGRAPHY.pWhite as any}>
         Ensure you have your mnemonic backup saved first!!
       </Text>
