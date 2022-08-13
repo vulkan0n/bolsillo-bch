9c57a9d079a3067098a767b1eed0741b94969183
JeremyEllingham <jeremy@sethindustries.com.au>
2022-08-13 10:44:54 +0100

    Refactor right handed mode.

--- src/components/NavigationTree/ToolsDrawerNavigator/SettingsView/index.tsx
+++ src/components/NavigationTree/ToolsDrawerNavigator/SettingsView/index.tsx
@@ -1,60 +1,15 @@
 import React from "react";
-import { View, Text, Switch } from "react-native";
-import TYPOGRAPHY from "../../../../design/typography";
+import { View } from "react-native";
 import styles from "./styles";
-import COLOURS from "../../../../design/colours";
-import { toggleIsRightHandedMode } from "../../../../redux/reducers/settingsReducer";
-import { useSelector, useDispatch } from "react-redux";
-import { ReduxState } from "../../../../types";
 import CryptoDenominated from "./CryptoDenominated";
 import TestNet from "./TestNet";
+import RightHandedMode from "./RightHandedMode";
 
 const SettingsView = () => {
-  const dispatch = useDispatch();
-  const { isRightHandedMode } = useSelector(
-    (state: ReduxState) => state.settings
-  );
-
-  const handleToggleIsRightHandedMode = () => {
-    dispatch(toggleIsRightHandedMode());
-  };
-
-  const RightHandedModeSwitch = (
-    <View style={styles.control}>
-      <Switch
-        trackColor={{ true: COLOURS.bchGreen, false: COLOURS.white }}
-        thumbColor={isRightHandedMode ? COLOURS.white : COLOURS.black}
-        ios_backgroundColor={COLOURS.lightGrey}
-        onValueChange={handleToggleIsRightHandedMode}
-        value={isRightHandedMode}
-      />
-    </View>
-  );
-
   return (
     <View style={styles.container as any}>
       <CryptoDenominated />
-
-      <View style={styles.optionRow as any}>
-        {!isRightHandedMode && RightHandedModeSwitch}
-        <View style={{ width: 250 }}>
-          <Text style={TYPOGRAPHY.h2Left as any}>Right Handed Mode</Text>
-          {isRightHandedMode && (
-            <Text style={TYPOGRAPHY.pWhiteLeft as any}>
-              Interactive components tend to appear on right of screen for easy
-              thumb access. Change if left-hand dominated.
-            </Text>
-          )}
-          {!isRightHandedMode && (
-            <Text style={TYPOGRAPHY.pWhiteLeft as any}>
-              Interactive components tend to appear on left of screen for easy
-              thumb access. Change if right-hand dominated.
-            </Text>
-          )}
-        </View>
-        {isRightHandedMode && RightHandedModeSwitch}
-      </View>
-
+      <RightHandedMode />
       <TestNet />
     </View>
   );
