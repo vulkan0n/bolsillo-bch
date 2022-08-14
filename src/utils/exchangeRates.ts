d8899bea2f65860ae5d2355884febe341ab0fc3e
JeremyEllingham <jeremy@sethindustries.com.au>
2022-08-14 19:43:38 +0100

    Add bitcoin denominations conversion method.

--- src/utils/exchangeRates.ts
+++ src/utils/exchangeRates.ts
@@ -2,13 +2,33 @@ import store from "../redux/store";
 import { useSelector } from "react-redux";
 import { ReduxState } from "../types";
 
-const SATS_PER_BCH = 100000000;
+const ONE_HUNDRED_MILLION = 100000000; // Sats per BCH
+const ONE_HUNDRED_THOUSAND = 100000; // Sats per mBCH
+const ONE_HUNDRED = 100; // Sats per bit
+
+export const convertPadBalanceInDenominationToSats = (
+  padBalance: string,
+  denomination: string
+) => {
+  switch (denomination) {
+    case "bitcoins":
+      return `${parseFloat(padBalance) * ONE_HUNDRED_MILLION}`;
+    case "millibitcoins":
+      return `${parseFloat(padBalance) * ONE_HUNDRED_THOUSAND}`;
+    case "bits":
+      return `${parseFloat(padBalance) * ONE_HUNDRED}`;
+    case "satoshis":
+      return padBalance;
+    default:
+      return padBalance;
+  }
+};
 
 export const convertSatsToUsd = (sats: string): string => {
   const { bchUsdPrice } = useSelector(
     (state: ReduxState) => state.exchangeRates
   );
   const equivalentUsd =
-    (parseFloat(sats) / SATS_PER_BCH) * parseFloat(bchUsdPrice);
+    (parseFloat(sats) / ONE_HUNDRED_MILLION) * parseFloat(bchUsdPrice);
   return `${equivalentUsd}`;
 };
