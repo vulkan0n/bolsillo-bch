fae52c9b7b4312c7b76dec9659fabfbcb121772c
JeremyEllingham <jeremy@sethindustries.com.au>
2022-09-23 14:32:32 +0100

    Extract TipWidget.

--- src/components/atoms/TipWidget/index.tsx
+++ src/components/atoms/TipWidget/index.tsx
@@ -1,11 +1,8 @@
 import React, { useState } from "react";
-import { View, ActivityIndicator, ScrollView, Text } from "react-native";
+import { View } from "react-native";
 import COLOURS from "@design/colours";
 import SPACING from "@design/spacing";
-import TYPOGRAPHY from "@design/typography";
-import YoutubePlayer from "react-native-youtube-iframe";
-import moment from "moment";
-import TipWidget from "@atoms/TipWidget";
+import Button from "@atoms/Button";
 import emit from "@utils/emit";
 import { BRIDGE_MESSAGE_TYPES } from "@utils/bridgeMessages";
 import { ReduxState } from "@types";
@@ -15,24 +12,11 @@ import { updateTransactionPadIsSendingCoins } from "@redux/reducers/transactionP
 import { selectActiveWalletBalance } from "@redux/selectors";
 
 interface Props {
-  title: string;
-  creator: string;
-  publicationDate: Date;
-  videoId: string;
-  description: string;
-  donationBchAddress?: string;
+  donationBchAddress: string;
 }
 
-function ContentCard({
-  title = "",
-  creator = "",
-  publicationDate = new Date(),
-  videoId = "",
-  description = "",
-  donationBchAddress,
-}) {
+function TipWidget({ donationBchAddress }: Props) {
   const [tipAmountInIntSats, setTipAmountInIntSats] = useState(100000);
-  const [isLoaded, setIsLoaded] = useState(false);
   const wallet = useSelector((state: ReduxState) => selectActiveWallet(state));
   const { isTestNet } = useSelector((state: ReduxState) => state.settings);
   const dispatch = useDispatch();
@@ -42,10 +26,6 @@ function ContentCard({
   const { availableRawSats } = useSelector((state: ReduxState) =>
     selectActiveWalletBalance(state)
   );
-
-  const onReady = () => {
-    setIsLoaded(true);
-  };
 
   const onPressTipBch = () => {
     if (!donationBchAddress) {
@@ -71,43 +51,33 @@ function ContentCard({
     );
   };
 
-  if (!isLoaded) {
-    <ActivityIndicator
-      style={{ height: 50 }}
-      size="large"
-      color={COLOURS.black}
-    />;
-  }
-
   return (
     <View
       style={{
         paddingTop: SPACING.five,
         marginVertical: SPACING.five,
-        backgroundColor: COLOURS.veryLightGrey,
-        borderColor: COLOURS.lightGrey,
-        borderWidth: 2,
-        borderRadius: SPACING.borderRadius,
       }}
     >
       <View
         style={{
-          marginHorizontal: SPACING.ten,
+          alignSelf: "center",
+          justifyContent: "center",
+          alignItems: "center",
+          width: 300,
         }}
       >
-        <Text style={TYPOGRAPHY.h2black as any}>{title}</Text>
-        <Text style={TYPOGRAPHY.p as any}>{creator}</Text>
-        <Text style={TYPOGRAPHY.p as any}>
-          {moment(publicationDate).format("ll")}
-        </Text>
+        <Button
+          onPress={onPressTipBch}
+          variant="primary"
+          icon={"faBitcoinSign"}
+          isLoading={isSendingCoins}
+          isDisabled={tipAmountInIntSats > parseInt(availableRawSats)}
+        >
+          Tip {`${tipAmountInIntSats}`} sats
+        </Button>
       </View>
-      <YoutubePlayer height={240} videoId={videoId} onReady={onReady} />
-      <Text style={TYPOGRAPHY.p as any}>{description}</Text>
-      {!!donationBchAddress && (
-        <TipWidget donationBchAddress={donationBchAddress} />
-      )}
     </View>
   );
 }
 
-export default ContentCard;
+export default TipWidget;
