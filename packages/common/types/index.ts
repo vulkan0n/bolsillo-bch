54dbffba7d8d461fc72abc0c772b21fc3301023f
JeremyEllingham <jeremy@sethindustries.com.au>
2022-09-27 08:33:34 +0100

    Relocating types.

---
+++ packages/common/types/index.ts
@@ -0,0 +1,69 @@
+// import { ExchangeRatesState } from "@redux/reducers/exchangeRatesReducer";
+// import { SettingsState } from "@redux/reducers/settingsReducer";
+// import { TransactionPadState } from "@redux/reducers/transactionPadReducer";
+// import { WalletManagerState } from "@redux/reducers/walletManagerReducer";
+// import { LocalState } from "@redux/reducers/localReducer";
+
+// export interface ReduxState {
+//   transactionPad: TransactionPadState;
+//   exchangeRates: ExchangeRatesState;
+//   walletManager: WalletManagerState;
+//   settings: SettingsState;
+//   local: LocalState;
+// }
+
+export interface WalletType {
+  mnemonic: string;
+  derivationPath: string;
+  cashaddr: string;
+}
+
+export interface TransactionType {
+  txn: string;
+  blockheight?: number;
+  note?: string;
+}
+
+export interface SeleneWalletType {
+  name: string;
+  description: string;
+  balance: string;
+  mnemonic: string;
+  derivationPath: string;
+  cashaddr: string;
+  transactions: TransactionType[];
+}
+
+export interface EmitEvent {
+  type: string;
+  data: {};
+}
+
+export type BitcoinDenominationTypes =
+  | "bitcoins"
+  | "millibits"
+  | "bits"
+  | "satoshis";
+
+export type SupportedCurrencyTypes =
+  | "aud"
+  | "btc"
+  | "cad"
+  | "cny"
+  | "eth"
+  | "eur"
+  | "gbp"
+  | "jpy"
+  | "php"
+  | "rub"
+  | "thb"
+  | "usd";
+
+export type CurrencyOrDenominationType =
+  | BitcoinDenominationTypes
+  | SupportedCurrencyTypes;
+
+export interface SupportedCurrency {
+  code: SupportedCurrencyTypes;
+  fullName: string;
+}
--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -3,14 +3,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import COLOURS from "@selene/common/design/colours";
 import SPACING from "@selene/common/design/spacing";
+import TYPOGRAPHY from "@selene/common/design/typography";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      
-      <p style={{ color: COLOURS.bchGreen }}>green text</p>
+
+      <p style={TYPOGRAPHY.h1black}>green text</p>
     </div>
   );
 }
