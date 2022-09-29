957dd3f0bbcc816cbcd6bf75f06acb73a8851aeb
JeremyEllingham <jeremy@sethindustries.com.au>
2022-09-29 07:25:46 +0100

    Copy tsconfig references.

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import TYPOGRAPHY from "@selene/common/design/typography";
 import Header from "./components/Header";
+import WebApp from "@selene/app/src/App";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <WebApp />
 
       <p style={TYPOGRAPHY.h1black}>green text</p>
     </div>
