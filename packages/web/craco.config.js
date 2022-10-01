e439e4aa1db033dc525c5e29d2745027cd7f3076
JeremyEllingham <jeremy@sethindustries.com.au>
2022-10-01 10:37:33 +0100

    Add babel preset.

--- packages/web/craco.config.js
+++ packages/web/craco.config.js
@@ -5,11 +5,15 @@ const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
 
 module.exports = {
+  babel: {
+    presets: ["@babel/preset-react"],
+  },
   plugins: [
     {
       plugin: require("craco-babel-loader"),
       options: {
         includes: [resolveApp("../common")],
+        includes: [resolveApp("../app")],
       },
     },
   ],
