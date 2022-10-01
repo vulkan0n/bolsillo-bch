0a6e6edb5f077c975673fa3408ac261cfc45c357
JeremyEllingham <jeremy@sethindustries.com.au>
2022-10-01 14:45:57 +0100

    Fix typescript errors.

--- packages/app/src/components/NavigationTree/rootNavigation.ts
+++ packages/app/src/components/NavigationTree/rootNavigation.ts
@@ -4,8 +4,11 @@ export const navigationRef = createNavigationContainerRef();
 
 // Allow navigation access outside Navigator tree
 // https://reactnavigation.org/docs/navigating-without-navigation-prop/
-export function navigate(name, params = {}) {
+// For some reason TypeScript wants these functions to be "never"
+export function navigate(name: string, params = {}) {
   if (navigationRef.isReady()) {
+    // For some weird reason navigationRef wants to be a "never" type
+    // @ts-ignore
     navigationRef.navigate(name, params);
   }
 }
