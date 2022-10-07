d6b46b872ec3786b9f9611fed417ca3ab4dd0500
JeremyEllingham <jeremy@sethindustries.com.au>
2022-10-07 10:02:30 +0100

    Update aesthetics of button row picker.

--- packages/app/src/components/NavigationTree/CommunityTabNavigator/StatsView/ActiveBitcoinersChart/index.tsx
+++ packages/app/src/components/NavigationTree/CommunityTabNavigator/StatsView/ActiveBitcoinersChart/index.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { View, Text, Pressable } from "react-native";
-import { useQuery, gql } from "@apollo/client";
 import moment from "moment";
 import { CHECK_IN_PERIOD_TYPES } from "@selene-wallet/common/dist/utils/consts";
 import Chart from "./Chart";
+import COLOURS from "@selene-wallet/common/design/colours";
+import TYPOGRAPHY from "@selene-wallet/common/design/typography";
+import SPACING from "@selene-wallet/common/design/spacing";
 
 const ActiveBitcoinersChart = ({ data, period, setPeriod }) => {
   const activeUserCount = data?.activeBitcoiners?.map?.(({ count }) => count);
@@ -17,43 +19,128 @@ const ActiveBitcoinersChart = ({ data, period, setPeriod }) => {
 
   return (
     <View>
-      {period !== CHECK_IN_PERIOD_TYPES.daily && (
+      <Chart data={activeUserCount} labels={labels} />
+      <View
+        style={{
+          height: 50,
+          width: "100%",
+          borderWidth: 1,
+          flexDirection: "row",
+          borderColor: COLOURS.lightGrey,
+          borderRadius: SPACING.borderRadius,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <Pressable
+          style={{
+            backgroundColor:
+              period === CHECK_IN_PERIOD_TYPES.daily
+                ? COLOURS.bchGreen
+                : COLOURS.white,
+            flex: 1,
+            width: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
           onPress={() => {
             setPeriod(CHECK_IN_PERIOD_TYPES.daily);
           }}
         >
-          <Text>Daily</Text>
+          <Text
+            style={{
+              ...TYPOGRAPHY.p,
+              color:
+                period === CHECK_IN_PERIOD_TYPES.daily
+                  ? COLOURS.white
+                  : COLOURS.bchGreen,
+            }}
+          >
+            Daily
+          </Text>
         </Pressable>
-      )}
-      {period !== CHECK_IN_PERIOD_TYPES.weekly && (
         <Pressable
+          style={{
+            backgroundColor:
+              period === CHECK_IN_PERIOD_TYPES.weekly
+                ? COLOURS.bchGreen
+                : COLOURS.white,
+            flex: 1,
+            width: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
           onPress={() => {
             setPeriod(CHECK_IN_PERIOD_TYPES.weekly);
           }}
         >
-          <Text>Weekly</Text>
+          <Text
+            style={{
+              ...TYPOGRAPHY.p,
+              color:
+                period === CHECK_IN_PERIOD_TYPES.weekly
+                  ? COLOURS.white
+                  : COLOURS.bchGreen,
+            }}
+          >
+            Weekly
+          </Text>
         </Pressable>
-      )}
-      {period !== CHECK_IN_PERIOD_TYPES.monthly && (
         <Pressable
+          style={{
+            backgroundColor:
+              period === CHECK_IN_PERIOD_TYPES.monthly
+                ? COLOURS.bchGreen
+                : COLOURS.white,
+            flex: 1,
+            width: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
           onPress={() => {
             setPeriod(CHECK_IN_PERIOD_TYPES.monthly);
           }}
         >
-          <Text>Monthly</Text>
+          <Text
+            style={{
+              ...TYPOGRAPHY.p,
+              color:
+                period === CHECK_IN_PERIOD_TYPES.monthly
+                  ? COLOURS.white
+                  : COLOURS.bchGreen,
+            }}
+          >
+            Monthly
+          </Text>
         </Pressable>
-      )}
-      {period !== CHECK_IN_PERIOD_TYPES.yearly && (
         <Pressable
+          style={{
+            backgroundColor:
+              period === CHECK_IN_PERIOD_TYPES.yearly
+                ? COLOURS.bchGreen
+                : COLOURS.white,
+            flex: 1,
+            width: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
           onPress={() => {
             setPeriod(CHECK_IN_PERIOD_TYPES.yearly);
           }}
         >
-          <Text>Yearly</Text>
+          <Text
+            style={{
+              ...TYPOGRAPHY.p,
+              color:
+                period === CHECK_IN_PERIOD_TYPES.yearly
+                  ? COLOURS.white
+                  : COLOURS.bchGreen,
+            }}
+          >
+            Yearly
+          </Text>
         </Pressable>
-      )}
-      <Chart data={activeUserCount} labels={labels} />
+      </View>
     </View>
   );
 };
