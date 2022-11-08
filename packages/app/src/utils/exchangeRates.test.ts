2512e4f645c081d5766225048302ca1bc20b8643
JeremyEllingham <jeremy@sethindustries.com.au>
2022-11-08 13:33:03 +0000

    Update test.

--- packages/app/src/utils/exchangeRates.test.ts
+++ packages/app/src/utils/exchangeRates.test.ts
@@ -5,7 +5,7 @@ test("Convert 0 raw bits to 0 raw sats", () => {
 });
 
 test("Convert 1 raw bit to 100 raw sats", () => {
-  expect(convertRawBitsToRawSats("10")).toBe("1000");
+  expect(convertRawBitsToRawSats("1")).toBe("100");
 });
 
 test("Convert 10 raw bits to 1 000 raw sats", () => {
