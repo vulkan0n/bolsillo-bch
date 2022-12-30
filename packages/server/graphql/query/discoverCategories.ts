6f6bf97f014399c025588a584c02b8fb574e70ed
JeremyEllingham <jeremy@sethindustries.com.au>
2022-12-30 23:06:52 +1100

    Add engineering resources.

--- packages/server/graphql/query/discoverCategories.ts
+++ packages/server/graphql/query/discoverCategories.ts
@@ -52,6 +52,80 @@ const discoverCategories = [
       },
     ],
   },
+  {
+    name: "Engineering Resources",
+    description: "Start building on Bitcoin Cash.",
+    items: [
+      {
+        name: "Developers.cash",
+        description:
+          "Useful developer resources for building the Internet of Cash.",
+        url: "https://developers.cash/",
+      },
+      {
+        name: "Reference.cash",
+        description: "Bitcoin Cash protocol specification reference.",
+        url: "https://reference.cash/",
+      },
+      {
+        name: "Libauth",
+        description:
+          " An ultra-lightweight JavaScript library for Bitcoin, Bitcoin Cash, and Bitauth applications.",
+        url: "https://github.com/bitauth/libauth",
+      },
+      {
+        name: "Chaingraph API",
+        description:
+          "Chaingraph is a multi-node blockchain indexer and GraphQL API.",
+        url: "https://chaingraph.cash/",
+      },
+      {
+        name: "Mainnet.cash",
+        description:
+          " Develop using Bitcoin Cash in JS or Python, Go, PHP, Ruby, etc - via REST.",
+        url: "https://mainnet.cash/",
+      },
+      {
+        name: "Bitcoin Cash Site / build",
+        description: "Collection of Bitcoin Cash building tools.",
+        url: "https://bitcoincashsite.com/build/",
+      },
+      {
+        name: "Fullstack.cash",
+        description:
+          "Adaptation of Mastering Bitcoin by Andreas Antonopoulos for technical reference to working with the Bitcoin Cash protocol.",
+        url: "https://fullstack.cash/documentation/",
+      },
+      {
+        name: "Mastering Bitcoin Cash",
+        description:
+          "Alternative adaptation of Mastering Bitcoin by Andreas Antonopoulos.",
+        url: "https://zh.thedev.id/mastering-bitcoin-cash/",
+      },
+      {
+        name: "CashStack",
+        description: "A JavaScript Framework for Sovereign Money and Data.",
+        url: "https://permissionless-software-foundation.github.io/cashstack.info/",
+      },
+      {
+        name: "Electrum Cash Protocol",
+        description:
+          "Electrum Cash Protocol¶ reference for client and server authors.",
+        url: "https://bitcoincash.network/electrum/",
+      },
+      {
+        name: "Awesome Bitcoin Cash",
+        description: "A curated list of Bitcoin Cash projects & resources.",
+        url: "https://github.com/2qx/awesome-bitcoin-cash",
+      },
+      {
+        name: "Unspent",
+        description:
+          "Decentralized finance using unspent transaction unlocking script. Open source, on-chain, running nativiely & directly on Bitcoin Cash (BCH).",
+        url: "https://unspent.app/",
+      },
+    ],
+  },
 ];
 
 module.exports = discoverCategories;
