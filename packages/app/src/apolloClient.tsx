11833e0996640dcfd52f6ed95a501864cd9a3c4e
JeremyEllingham <jeremy@sethindustries.com.au>
2023-01-18 12:16:14 +0000

    Prep app release v0.1.0.

--- packages/app/src/apolloClient.tsx
+++ packages/app/src/apolloClient.tsx
@@ -5,8 +5,8 @@ const PRODUCTION_SERVER = "https://stats.selene.cash/";
 const LOCALHOST_SERVER = "http://localhost:4000/";
 
 const apolloClient = new ApolloClient({
-  uri: LOCALHOST_SERVER,
-  // uri: PRODUCTION_SERVER,
+  // uri: LOCALHOST_SERVER,
+  uri: PRODUCTION_SERVER,
   cache: new InMemoryCache(),
 });
 
