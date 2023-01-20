f705a62cba89efbdf19bb8695d053ee84d7d10b6
JeremyEllingham <jeremy@sethindustries.com.au>
2023-01-20 05:22:46 +0000

    Add yalc packages and improve eas builds.

--- packages/app/metro.config.js
+++ packages/app/metro.config.js
@@ -3,7 +3,7 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = {
+const updatedConfig = {
   ...config,
   transformer: {
     ...config.transformer,
@@ -11,4 +11,22 @@ module.exports = {
       "react-native-react-bridge/lib/plugin"
     ),
   },
+  resolver: {
+    ...config.resolver,
+    alias: {
+      // https://mainnet.cash/tutorial/shipping-mainnet.html#configuring-webpack
+      // mainnet-js
+      // Substituting react-native-tcp-socket for net / tls
+      stream: require.resolve("stream-browserify"), // bip39
+      crypto: require.resolve("crypto-browserify"), // bip39
+      net: require.resolve("react-native-tcp-socket"), // electrum-cash tcp connections
+      tls: require.resolve("react-native-tcp-socket"), // electrum-cash tcp connections
+      fs: false, // qrcode-svg.save
+    },
+  },
 };
+
+// https://github.com/apollographql/apollo-client/issues/9194#issuecomment-1000850769
+updatedConfig.resolver.sourceExts.push("cjs");
+
+module.exports = updatedConfig;
