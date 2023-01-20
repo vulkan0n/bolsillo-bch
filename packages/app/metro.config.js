56ce8915ca66149d31a143f7b84d6d2590d51c63
JeremyEllingham <jeremy@sethindustries.com.au>
2023-01-20 18:47:30 +0000

    Update metro config.

--- packages/app/metro.config.js
+++ packages/app/metro.config.js
@@ -23,6 +23,8 @@ const updatedConfig = {
       tls: require.resolve("react-native-tcp-socket"), // electrum-cash tcp connections
       fs: require.resolve("react-native-tcp-socket"), // qrcode-svg.save
     },
+    // Allow local use of yalc without messing up the build pipeline
+    blacklistRE: exclusionList([/.yalc\/.*/]),
   },
   // To fix an issue with metro caching of @babel/runtime
   // https://github.com/facebook/react-native/issues/27712#issuecomment-715780864
