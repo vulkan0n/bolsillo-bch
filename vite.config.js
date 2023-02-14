8990b8e76602f4ab07be86e3d1f75dd48d6ec1f2
Kallisti <kallisti@kallisti.io>
2023-02-14 18:08:44 -0500

    fix libauth/mainnet/pg-format build error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+import topLevelAwait from "vite-plugin-top-level-await";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+// need to 'include' pg-format because of a weird problem with the require it has
 export default defineConfig({
-  plugins: [react()],
-})
+  optimizeDeps: {
+    include: ["pg-format"],
+  },
+  plugins: [
+    react(),
+    nodePolyfills(),
+    topLevelAwait({
+      promiseExportName: "__tla",
+      promiseImportName: (i) => `__tla_${i}`,
+    }),
+  ],
+});
