f560e1d9214ee79fcda012d294917d8b0c58d333
Kallisti <kallisti@kallisti.io>
2023-02-14 21:31:28 -0500

    remove pg-format workaround

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,7 @@ import topLevelAwait from "vite-plugin-top-level-await";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-// need to 'include' pg-format because of a weird problem with the require it has
 export default defineConfig({
-  optimizeDeps: {
-    include: ["pg-format"],
-  },
   plugins: [
     react(),
     nodePolyfills(),
