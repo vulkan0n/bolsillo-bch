7974b9fca1c848a19fa325b9fc7bb0f3b23308d4
Kallisti <kallisti@kallisti.io>
2023-02-16 18:03:21 -0500

    more walletview styles

--- src/components/views/walletView/WalletViewReceive.jsx
+++ src/components/views/walletView/WalletViewReceive.jsx
@@ -22,21 +22,26 @@ function WalletViewReceive() {
   const skipAddress = () => setSkip((skip + 1) % 5);
 
   return (
-    <div className="py-2">
+    <div className="py-2 my-4">
       <div className="flex justify-center">
-        <QRCode value={address} size={200} quietZone={16} />
+        <div className="border border-4 border-zinc-300 my-2">
+          <QRCode value={address} size={200} quietZone={16} />
+        </div>
       </div>
       <div
         onClick={copyAddressToClipboard}
-        className="text-sm text-center break-all cursor-pointer"
+        className="text-sm text-center break-all cursor-pointer py-1"
       >
         {formattedAddress}
+      </div>
+      {/*<div className="text-xs text-center break-word uppercase text-zinc-400">
+        Only send Bitcoin Cash (BCH) to this address
       </div>
       <div className="flex justify-center my-1">
         <button className="btn btn-xs" type="button" onClick={skipAddress}>
           Cycle Address
         </button>
-      </div>
+      </div>*/}
     </div>
   );
 }
