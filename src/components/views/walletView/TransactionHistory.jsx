fb644541b8b86472e8966d2a2ce9af090a9c836a
Kallisti <kallisti@kallisti.io>
2023-02-16 21:05:00 -0500

    add TransactionHistory component

---
+++ src/components/views/walletView/TransactionHistory.jsx
@@ -0,0 +1,72 @@
+function TransactionHistory() {
+  return (
+    <table className="table table-compact w-full rounded-none">
+      <thead>
+        <tr>
+          <th>Time</th>
+          <th>To</th>
+          <th>Amount</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>A few seconds ago</td>
+          <td>qzyw7-mm6d</td>
+          <td>-1.45163248</td>
+        </tr>
+        <tr>
+          <td>5 minutes ago</td>
+          <td>You</td>
+          <td>+0.30153702</td>
+        </tr>
+        <tr>
+          <td>12 minutes ago</td>
+          <td>You</td>
+          <td>+5.01431754</td>
+        </tr>
+        <tr>
+          <td>15:42</td>
+          <td>qzx5d-5hp9</td>
+          <td>-0.00133703</td>
+        </tr>
+        <tr>
+          <td>Yesterday</td>
+          <td>qq354-9etp</td>
+          <td>-0.42051791</td>
+        </tr>
+        <tr>
+          <td>2023-02-14</td>
+          <td>You</td>
+          <td>+0.97143219</td>
+        </tr>
+        <tr>
+          <td>2023-02-13</td>
+          <td>qqpkak-zuk5</td>
+          <td>-0.74647216</td>
+        </tr>
+        <tr>
+          <td>2023-02-12</td>
+          <td>You</td>
+          <td>+3.74825931</td>
+        </tr>
+        <tr>
+          <td>2023-02-12</td>
+          <td>qzx5d-5hp9</td>
+          <td>-0.00133703</td>
+        </tr>
+        <tr>
+          <td>2023-02-12</td>
+          <td>qq354-9etp</td>
+          <td>-0.42051791</td>
+        </tr>
+        <tr>
+          <td>2023-02-11</td>
+          <td>You</td>
+          <td>+999.74825931</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+export default TransactionHistory;
