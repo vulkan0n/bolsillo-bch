5bb363b69557343be282b4620d2f66787cc0277b
Kallisti <kallisti@kallisti.io>
2023-04-15 16:46:09 -0400

    initial sync indicator component

--- src/components/views/WalletView.jsx
+++ src/components/views/WalletView.jsx
@@ -1,15 +1,22 @@
 import { Outlet } from "react-router-dom";
 import WalletViewBalance from "./walletView/WalletViewBalance";
 import WalletViewTabs from "./walletView/WalletViewTabs";
+import SyncIndicator from "./walletView/SyncIndicator";
 
-function WalletView() {
+export default function WalletView() {
   return (
     <>
-      <WalletViewBalance />
+      <div className="grid grid-cols-6 bg-zinc-900">
+        <div className="col-span-1">&nbsp;</div>
+        <div className="col-span-4">
+          <WalletViewBalance />
+        </div>
+        <div className="col-span-1">
+          <SyncIndicator />
+        </div>
+      </div>
       <WalletViewTabs />
       <Outlet />
     </>
   );
 }
-
-export default WalletView;
---
+++ src/components/views/walletView/SyncIndicator.jsx
@@ -0,0 +1,7 @@
+export default function SyncIndicator() {
+  return (
+    <div className="flex items-center h-full">
+      <div className="flex-1 text-primary"></div>
+    </div>
+  )
+};
