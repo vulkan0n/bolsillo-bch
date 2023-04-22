6d73f492e20279afd970e7a9dd67c9af325cdb3f
Kallisti <kallisti@kallisti.io>
2023-04-22 03:09:22 -0400

    add FiatOracleService

---
+++ src/services/FiatOracleService.jsx
@@ -0,0 +1,38 @@
+import { bchToSats, satsToBch } from "@/util/sats";
+import { Decimal } from "decimal.js";
+import { selectPreferences } from "@/redux/preferences";
+import { store } from "@/redux";
+
+export default function FiatOracleService() {
+  const preferences = selectPreferences(store.getState());
+
+  const oracles = {
+    USD: "125.02",
+    CAD: "125.04",
+    EUR: "125.03",
+    BTC: "0.00543210",
+  };
+
+  return {
+    toSats,
+    toBch,
+    toFiat,
+  };
+
+  function toSats(amount) {
+    return bchToSats(
+      new Decimal(1 / oracles[preferences["localCurrency"]]).times(amount)
+    );
+  }
+
+  function toBch(amount) {
+    return new Decimal(satsToBch(toSats(amount))).toDecimalPlaces(8).toString();
+  }
+
+  function toFiat(sats) {
+    return new Decimal(satsToBch(sats))
+      .times(oracles[preferences["localCurrency"]])
+      .toDecimalPlaces(2)
+      .toString();
+  }
+}
