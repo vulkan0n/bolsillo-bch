5f63007e5c4ff04fcf248df69a965b12ef385a7e
Kallisti <kallisti@kallisti.io>
2023-05-30 00:52:01 -0400

    simplify toast html

--- src/util/toast.jsx
+++ src/util/toast.jsx
@@ -3,18 +3,14 @@ import toast from "react-hot-toast";
 const showToast = ({ icon = <></>, title = "", description = "" }) => {
   toast.custom((t) => (
     <div
-      className={`animate-enter max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+      className={`animate-enter max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5 p-2`}
     >
-      <div className="flex-1 w-0 p-4">
-        <div className="flex items-start">
-          <div className="flex-shrink-0 pt-0.5">{icon}</div>
-          <div className="ml-3">
-            <p className="text-md font-medium text-gray-900">{title}</p>
-            <p className="mt-1 text-sm break-all text-zinc-500 ">
-              {description}
-            </p>
-          </div>
-        </div>
+      <div className="flex-shrink-0 my-auto p-1">
+        <div className="flex items-center justify-center">{icon}</div>
+      </div>
+      <div className="p-2">
+        <p className="text-lg font-medium text-gray-900">{title}</p>
+        <p className="text-md break-all text-zinc-600">{description}</p>
       </div>
     </div>
   ));
