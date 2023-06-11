fe2f15625869a8ec373340d6583f2730d3bdbc61
Kallisti <kallisti@kallisti.io>
2023-06-11 03:22:41 -0400

    update wallet view, buttons, scanner overlay

---
+++ src/components/atoms/Button.jsx
@@ -0,0 +1,35 @@
+export default function Button({
+  icon,
+  label,
+  onClick,
+  size = "12",
+  iconSize = "2xl",
+  labelSize = "sm",
+  labelColor = "zinc-700",
+  inverted,
+}) {
+  const Icon = icon;
+  const inactiveClasses =
+    "bg-white text-zinc-600 border-zinc-100 active:bg-primary active:border-primary active:text-white";
+  const activeClasses =
+    "bg-primary border-primary text-white active:bg-white active:text-zinc-600 active:border-zinc-100";
+  const colorClasses = inverted ? activeClasses : inactiveClasses;
+
+  return (
+    <div className="text-center">
+      <button type="button" onClick={onClick} className={`w-${size} h-${size}`}>
+        <div
+          className={`w-full h-full max-w-fit flex items-center justify-center cursor-pointer p-3 mx-auto
+        rounded-full outline outline-2 outline-primary shadow-md border opacity-90 
+        ${colorClasses}
+        active:shadow-none active:shadow-inner`}
+        >
+          <Icon className={`text-${iconSize}`} />
+        </div>
+      </button>
+      <div className={`text-${labelSize} text-${labelColor} mt-1 select-none`}>
+        {label}
+      </div>
+    </div>
+  );
+}
