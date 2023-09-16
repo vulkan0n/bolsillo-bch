ef61a208827113d1daa15f2dbb73476cbcc55e66
Kallisti <kallisti@kallisti.io>
2023-09-16 00:10:43 -0400

    address list

--- src/components/views/wallet/assets/WalletAssetsView.jsx
+++ src/components/views/wallet/assets/WalletAssetsView.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import { useSelector } from "react-redux";
 import { selectActiveWallet } from "@/redux/wallet";
 import { selectCurrencySettings } from "@/redux/preferences";
@@ -7,7 +8,6 @@ import { formatSatoshis } from "@/util/sats";
 
 export default function WalletAssetsView() {
   const wallet = useSelector(selectActiveWallet);
-  const { shouldPreferLocalCurrency } = useSelector(selectCurrencySettings);
 
   const AddressManager = new AddressManagerService(wallet.id);
   const receiveAddresses = AddressManager.getReceiveAddresses();
@@ -15,70 +15,54 @@ export default function WalletAssetsView() {
 
   return (
     <div className="p-2">
-      <div className="bg-zinc-200 rounded my-1 p-1">
-        <h2 className="text-xl font-bold">Receive Addresses</h2>
-        <ul className="bg-zinc-100 text-zinc-600 divide-y divide-zinc-300 rounded-b px-2 max-h-[58vh] overflow-y-scroll border border-zinc-400 shadow-inner">
-          {receiveAddresses.map((a, i) => (
-            <li key={a.address} className="flex px-1 py-2">
-              <div
-                className="flex-1 text-sm"
-                onClick={() => null /*console.log(tx.time, new Date(tx.time))*/}
-              >
-                <span className="font-mono text-xs mr-1">#{a.hd_index}</span>
-                <Address address={a.address} short />
-              </div>
-              <div className="flex-1 text-right">
-                <div className="font-mono">
-                  {
-                    formatSatoshis(a.balance)[
-                      shouldPreferLocalCurrency ? "fiat" : "bch"
-                    ]
-                  }
-                </div>
-                <div className="text-sm opacity-80">
-                  {
-                    formatSatoshis(a.balance)[
-                      shouldPreferLocalCurrency ? "bch" : "fiat"
-                    ]
-                  }
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
+      <div className="bg-zinc-200 rounded my-1 p-2">
+        <h2 className="text-xl font-bold mb-2">Receive Addresses</h2>
+        <AddressBlock addresses={receiveAddresses} />
       </div>
       <div className="bg-zinc-200 rounded my-1 p-1">
         <h2 className="text-xl font-bold">Change Addresses</h2>
-        <ul className="bg-zinc-100 text-zinc-600 divide-y divide-zinc-300 rounded-b px-2 max-h-[58vh] overflow-y-scroll border border-zinc-400 shadow-inner">
-          {changeAddresses.map((a, i) => (
-            <li key={a.address} className="flex px-1 py-2">
-              <div
-                className="flex-1 text-sm"
-                onClick={() => null /*console.log(tx.time, new Date(tx.time))*/}
-              >
-                <span className="font-mono text-xs mr-1">#{a.hd_index}</span>
-                <Address address={a.address} short />
-              </div>
-              <div className="flex-1 text-right">
-                <div className="font-mono">
-                  {
-                    formatSatoshis(a.balance)[
-                      shouldPreferLocalCurrency ? "fiat" : "bch"
-                    ]
-                  }
-                </div>
-                <div className="text-sm opacity-80">
-                  {
-                    formatSatoshis(a.balance)[
-                      shouldPreferLocalCurrency ? "bch" : "fiat"
-                    ]
-                  }
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
+        <AddressBlock addresses={changeAddresses} />
       </div>
     </div>
   );
 }
+
+function AddressBlock({ addresses }) {
+  const { shouldPreferLocalCurrency } = useSelector(selectCurrencySettings);
+
+  return (
+    <ul className="bg-zinc-100 text-zinc-600 divide-y divide-zinc-300 rounded-sm px-2 max-h-[58vh] overflow-y-scroll border border-zinc-400 shadow-inner">
+      {addresses.map((a) => (
+        <li key={a.address} className="flex px-1 py-2">
+          <div
+            className="flex-1 text-sm"
+            onClick={() => null /*console.log(tx.time, new Date(tx.time))*/}
+          >
+            <span className="font-mono text-xs mr-1">#{a.hd_index}</span>
+            <Address address={a.address} short />
+          </div>
+          <div className="flex-1 text-right">
+            <div className="font-mono">
+              {
+                formatSatoshis(a.balance)[
+                  shouldPreferLocalCurrency ? "fiat" : "bch"
+                ]
+              }
+            </div>
+            <div className="text-sm opacity-80">
+              {
+                formatSatoshis(a.balance)[
+                  shouldPreferLocalCurrency ? "bch" : "fiat"
+                ]
+              }
+            </div>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+AddressBlock.propTypes = {
+  addresses: PropTypes.array.isRequired,
+};
