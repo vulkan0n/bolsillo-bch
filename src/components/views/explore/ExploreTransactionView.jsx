df8c08f3bec7550a06592ad28858929d5f1acd51
Kallisti <kallisti@kallisti.io>
2023-11-09 21:04:08 -0500

    more tx detail work

--- src/components/views/explore/ExploreTransactionView.jsx
+++ src/components/views/explore/ExploreTransactionView.jsx
@@ -1,27 +1,78 @@
 import { useLoaderData } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectCurrencySettings } from "@/redux/preferences";
 //import {} from "@ant-design/icons";
 
-import TransactionManagerService from "@/services/TransactionManagerService";
+import Address from "@/atoms/Address";
+import { formatSatoshis } from "@/util/sats";
 
 export default function ExploreTransactionView() {
   const tx = useLoaderData();
+  const { shouldPreferLocalCurrency } = useSelector(selectCurrencySettings);
+
+  const isConfirmed = tx.blocktime !== "null";
+  const txDate = (
+    isConfirmed ? new Date(tx.blocktime) : Date.parse(tx.time_seen)
+  ).toLocaleString();
+
+  const receiveStyle = "text-secondary";
+  const sendStyle = "text-error";
 
   return (
-    <div className="p-2">
-      <div className="bg-zinc-100">
-        <div className="text-lg font-semibold">Transaction Hash</div>
-        <div className="break-all font-mono">{tx.txid}</div>
+    <>
+      <div className="p-2 bg-zinc-100 border-b border-zinc-400">
+        <span className="text-lg font-semibold mr-1">Transaction ID:</span>
+        <span className="break-all font-mono text-sm">{tx.txid}</span>
       </div>
-      <div className="bg-zinc-100 rounded font-mono">
-        <ul>
-          {Object.keys(tx).map((key) => (
-            <li>
-              <span className="font-bold">{key}:</span>{" "}
-              {JSON.stringify(tx[key])}
-            </li>
-          ))}
-        </ul>
+      <div className="text-zinc-800 p-1 font-bold">
+        {isConfirmed ? "Confirmed" : "Seen"} {txDate}
       </div>
-    </div>
+      <div className="p-1">
+        <div className="bg-zinc-200 rounded p-1">
+          <div className="font-semibold py-1">Outputs</div>
+          <ul>
+            {tx.vout.map((output) => (
+              <li>
+                <div className="flex">
+                  <div>#{output.n}</div>
+                  <div className="text-sm tracking-tighter">
+                    <Address address={output.scriptPubKey.addresses[0]} short />
+                  </div>
+                  <div className="flex-1 text-right">
+                    <div
+                      className={`font-mono ${
+                        output.value > 0 ? receiveStyle : sendStyle
+                      }`}
+                    >
+                      {output.value > 0 && "+"}
+                      {
+                        formatSatoshis(output.value)[
+                          shouldPreferLocalCurrency ? "fiat" : "bch"
+                        ]
+                      }
+                    </div>
+                    <div className="text-sm opacity-80">
+                      {
+                        formatSatoshis(output.value)[
+                          shouldPreferLocalCurrency ? "bch" : "fiat"
+                        ]
+                      }
+                    </div>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <div>Inputs</div>
+          <ul>
+            {tx.vin.map((input) => (
+              <li>{JSON.stringify(input)}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
   );
 }
