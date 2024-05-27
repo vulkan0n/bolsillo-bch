59dc714ad66223f87b0f489707a5bedb35104e6c
Kallisti <kallisti@kallisti.io>
2024-05-26 21:24:02 -0400

    leafet sketch

--- src/components/views/explore/map/ExploreMapView.jsx
+++ src/components/views/explore/map/ExploreMapView.jsx
@@ -1,7 +1,24 @@
+import { Geolocation } from "@capacitor/geolocation";
+import { MapContainer, TileLayer } from "react-leaflet";
+
 export default function ExploreMapView() {
+  const printCurrentPosition = async () => {
+    const coordinates = await Geolocation.getCurrentPosition({
+      enableHighAccuracy: true,
+    });
+
+    console.log("Current position:", coordinates);
+  };
+
   return (
     <div>
-      <div>ExploreMapView</div>
+      <button onClick={printCurrentPosition}>Get Coordinates</button>
+      <MapContainer>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
     </div>
   );
 }
