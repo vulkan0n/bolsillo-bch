84906518af39f6869cdee8884eef22989d6bcb5a
Jeremy <jeremy@Admins-MacBook-Pro.local>
2024-06-17 18:50:17 +0100

    Better calculation and display of active user counts.

--- src/components/views/explore/ExploreStatBlock.jsx
+++ src/components/views/explore/ExploreStatBlock.jsx
@@ -19,9 +19,36 @@ export default function ExploreStatBlock() {
     return stopPolling;
   }, [startPolling, stopPolling]);
 
-  console.log({
-    data
-  })
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
+
+  const {
+    dailyActiveCount,
+    dailyActivePreviousCount,
+    weeklyActiveCount,
+    weeklyActivePreviousCount,
+    monthlyActiveCount,
+    monthlyActivePreviousCount,
+    yearlyActiveCount,
+    yearlyActivePreviousCount,
+  } = data.activeBitcoinersSummary
+
+  const dailyChange = dailyActiveCount - dailyActivePreviousCount
+  const dailyChangePercentage = 100 / dailyActiveCount * dailyChange
+  const isDailyActiveIncrease = dailyChange >= 0
+
+  const weeklyChange = weeklyActiveCount - weeklyActivePreviousCount
+  const weeklyChangePercentage = 100 / weeklyActiveCount * weeklyChange
+  const isWeeklyActiveIncrease = weeklyChange >= 0
+
+  const monthlyChange = monthlyActiveCount - monthlyActivePreviousCount
+  const monthlyChangePercentage = 100 / monthlyActiveCount * monthlyChange
+  const isMonthlyActiveIncrease = monthlyChange >= 0
+
+  const yearlyChange = yearlyActiveCount - yearlyActivePreviousCount
+  const yearlyChangePercentage = 100 / yearlyActiveCount * yearlyChange
+  const isYearlyActiveIncrease = yearlyChange >= 0
 
   return (
     <div className="shadow rounded-lg p-2 bg-zinc-900 w-full flex-column justify-between items-center" >
@@ -31,39 +58,52 @@ export default function ExploreStatBlock() {
       <div className="shadow rounded-lg p-2 bg-zinc-900 w-full flex justify-between items-center">
         <Link to="/explore/stats/#d">
           <div className="p-1 mx-1">
-            <div className="font-bold text-zinc-300">Last 24h</div>
-            <div className="text-primary text-lg font-semibold">
-              {isLoading && "..."}
-              {!isLoading && "1337"}
+            <div className="font-bold text-zinc-300">Daily</div>
+            <div className={`${isDailyActiveIncrease ? "text-primary" : "text-red-500"} text-lg font-semibold`}>
+              {dailyActiveCount}
             </div>
-            <div className="text-xs text-zinc-400">↗︎ 42 (3%)</div>
+            <div className={`text-xs ${isDailyActiveIncrease ? "text-green-400" : "text-red-400"}`}>
+              {isDailyActiveIncrease ? "↑" : "↓"} {isDailyActiveIncrease && "+"}{dailyChange} ({isDailyActiveIncrease && "+"}{dailyChangePercentage}%)
+            </div>
           </div>
         </Link>
 
         <Link to="/explore/stats/#w">
           <div className="p-1 mx-1">
-            <div className="font-bold text-zinc-300">Last 7d</div>
-            <div className="text-primary text-lg font-semibold">23,505</div>
-            <div className="text-xs text-zinc-400">↗︎ 42 (3%)</div>
+            <div className="font-bold text-zinc-300">Weekly</div>
+            <div className={`${isWeeklyActiveIncrease ? "text-primary" : "text-red-500"} text-lg font-semibold`}>
+              {weeklyActiveCount}
+            </div>
+            <div className={`text-xs ${isWeeklyActiveIncrease ? "text-green-400" : "text-red-400"}`}>
+              {isWeeklyActiveIncrease ? "↑" : "↓"} {isWeeklyActiveIncrease && "+"}{weeklyChange} ({isWeeklyActiveIncrease && "+"}{weeklyChangePercentage}%)
+            </div>
           </div>
         </Link>
 
-        <Link to="/explore/stats/#m">
+        <Link to="/explore/stats/#w">
           <div className="p-1 mx-1">
-            <div className="font-bold text-zinc-300">Last 30d</div>
-            <div className="text-primary text-lg font-semibold">23,505</div>
-            <div className="text-xs text-zinc-400">↗︎ 42 (3%)</div>
+            <div className="font-bold text-zinc-300">Monthly</div>
+            <div className={`${isMonthlyActiveIncrease ? "text-primary" : "text-red-500"} text-lg font-semibold`}>
+              {monthlyActiveCount}
+            </div>
+            <div className={`text-xs ${isMonthlyActiveIncrease ? "text-green-400" : "text-red-400"}`}>
+              {isMonthlyActiveIncrease ? "↑" : "↓"} {isMonthlyActiveIncrease && "+"}{monthlyChange} ({isMonthlyActiveIncrease && "+"}{monthlyChangePercentage}%)
+            </div>
           </div>
         </Link>
 
         <Link to="/explore/stats/#y">
           <div className="p-1 mx-1">
-            <div className="font-bold text-zinc-300">Last 365d</div>
-            <div className="text-primary text-lg font-semibold">23,505</div>
-            <div className="text-xs text-zinc-400">↗︎ $0.42 (3%)</div>
+            <div className="font-bold text-zinc-300">Yearly</div>
+            <div className={`${isYearlyActiveIncrease ? "text-primary" : "text-red-500"} text-lg font-semibold`}>
+              {yearlyActiveCount}
+            </div>
+            <div className={`text-xs ${isYearlyActiveIncrease ? "text-green-400" : "text-red-400"}`}>
+              {isYearlyActiveIncrease ? "↑" : "↓"} {isYearlyActiveIncrease && "+"}{yearlyChange} ({isYearlyActiveIncrease && "+"}{yearlyChangePercentage}%)
+            </div>
           </div>
         </Link>
-      </div>
-    </div>
+      </div >
+    </div >
   );
 }
--- src/components/views/explore/stats/getActiveBitcoinersSummary.js
+++ src/components/views/explore/stats/getActiveBitcoinersSummary.js
@@ -4,14 +4,15 @@ const GET_ACTIVE_BITCOINERS_SUMMARY = gql`
   query GetActiveBitcoinersSummary {
     activeBitcoinersSummary {
       dailyActiveCount
-      dailyActiveChange
+      dailyActivePreviousCount
+      weeklyActiveCount
+      weeklyActivePreviousCount
+      monthlyActiveCount
+      monthlyActivePreviousCount
+      yearlyActiveCount
+      yearlyActivePreviousCount
     }
   }
 `;
-
-// 30dActive
-// 30dChange
-// 365dActive
-// 365dChange
 
 export default GET_ACTIVE_BITCOINERS_SUMMARY;
