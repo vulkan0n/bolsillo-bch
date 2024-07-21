8bca010b8a51beeb0dcdf2a23377888ca1e98c82
Jeremy <jeremy@Admins-MacBook-Pro.local>
2024-07-21 09:41:11 +0200

    Restyling chronology.

--- src/components/views/explore/chronology/ExploreChronologyView.jsx
+++ src/components/views/explore/chronology/ExploreChronologyView.jsx
@@ -1,11 +1,5 @@
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import {
-  ContactsOutlined,
-  EnvironmentOutlined,
-  ProfileOutlined,
-  QuestionCircleOutlined,
-} from "@ant-design/icons";
 
 export default function ExploreChronologyView() {
   return (
@@ -14,53 +8,26 @@ export default function ExploreChronologyView() {
 
       <VerticalTimeline
         lineColor={'black'}
+        layout={'1-column-left'}
       >
+        <div className={"pl-10"}>
+          <p>2011</p>
+        </div>
+
         <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff', paddingTop: 2 }}
           contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-          date="2011 - present"
           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          icon={<ContactsOutlined />}
+          icon={null}
         >
-          <h3 className="vertical-timeline-element-title">Creative Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-          <p>
-            Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-          </p>
+          <p className={"bg-zinc-500 pt-0 mt-0"}>My own custom text</p>
         </VerticalTimelineElement>
-
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           date="2010 - 2011"
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          icon={<EnvironmentOutlined />}
-        >
-          <h3 className="vertical-timeline-element-title">Art Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-          <p>
-            Creative Direction, User Experience, Visual Design, SEO, Online Marketing
-          </p>
-        </VerticalTimelineElement>
-
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="2010 - 2011"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          icon={<EnvironmentOutlined />}
-        >
-          <h3 className="vertical-timeline-element-title">Art Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-          <p>
-            Creative Direction, User Experience, Visual Design, SEO, Online Marketing
-          </p>
-        </VerticalTimelineElement>
-
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="2010 - 2011"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          icon={<EnvironmentOutlined />}
+          icon={null}
         >
           <h3 className="vertical-timeline-element-title">Art Director</h3>
           <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
