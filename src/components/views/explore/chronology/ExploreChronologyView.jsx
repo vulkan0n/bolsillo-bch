012bd2f2e350dda2f0601c382ed3272ca8de1f52
Jeremy <jeremy@Admins-MacBook-Pro.local>
2024-07-21 12:33:57 +0200

    Styling Chronology.

--- src/components/views/explore/chronology/ExploreChronologyView.jsx
+++ src/components/views/explore/chronology/ExploreChronologyView.jsx
@@ -11,16 +11,34 @@ export default function ExploreChronologyView() {
         layout={'1-column-left'}
       >
         <div className={"pl-10"}>
-          <p>2011</p>
+          <p>2024</p>
         </div>
 
         <VerticalTimelineElement
-          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff', paddingTop: 2 }}
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff', paddingTop: 0, paddingBottom: 0 }}
           contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
           icon={null}
         >
-          <p className={"bg-zinc-500 pt-0 mt-0"}>My own custom text</p>
+          <div className="pb-1" >
+            <div className="flex justify-between items-center pt-4">
+              <span className="text-xl font-bold">ABLA</span>
+              <span className={"text text-zinc-200 pt-0 mt-2 pb-2"}>15th May 2024</span>
+            </div>
+            <span className="text-md text-white-800 pt-4">Adjustable Blocksize Limit Algorithm</span>
+          </div>
+          <div>
+            <span className={"text pt-0 mt-0"}>BCH's 32 MB blocksize limit replaced with a new adjustable limit that responds intelligently to network traffic.</span>
+          </div>
+          <div className={"pb-1"}>
+            <span className={"text pt-0 mt-0"}>Upgrade was celebrated live in Ljubljana, Slovenia at BCH BLISS.</span>
+          </div>
+          <hr />
+          <br />
+          <hr />
+          <div className="flex justify-between" >
+            <span className={"text text-zinc-200 pt-0 mt-2 pb-2"}>Protocol Upgrade (Hard Fork)</span>
+          </div>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
