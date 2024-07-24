a84c3c77ca914d89e8298add1f93a0f10013f7eb
Jeremy <jeremy@Admins-MacBook-Pro.local>
2024-07-24 23:22:00 +0200

    Fix ApolloProvider import.

--- src/components/views/explore/stats/ExploreStatsView.jsx
+++ src/components/views/explore/stats/ExploreStatsView.jsx
@@ -2,36 +2,19 @@ import { useState, useEffect } from "react";
 import { useQuery } from "@apollo/client";
 import GET_ACTIVE_BITCOINERS from "./getActiveBitcoiners";
 import { Period, THIRTY_SECONDS } from "@/util/time";
+import { ApolloProvider } from "@apollo/client";
+import apolloClient from "../../../../../src/apolloClient";
 
 import ExploreStatBlock from "./ExploreStatBlock";
 import GlobalAdoptionSummary from "./GlobalAdoptionSummary";
 
 export default function ExploreStatsView() {
-  const [period, setPeriod] = useState(Period.Daily);
-
-  const {
-    loading: isLoading,
-    data,
-    startPolling,
-    stopPolling,
-  } = useQuery(GET_ACTIVE_BITCOINERS, {
-    variables: {
-      period,
-    },
-  });
-
-  useEffect(() => {
-    startPolling(THIRTY_SECONDS);
-
-    return stopPolling;
-  }, [startPolling, stopPolling]);
-
-  const isReady = !isLoading && data?.activeBitcoiners;
-
   return (
-    <div className="p-2">
-      <ExploreStatBlock />
-      <GlobalAdoptionSummary />
-    </div>
+    <ApolloProvider client={apolloClient}>
+      <div className="p-2">
+        <ExploreStatBlock />
+        <GlobalAdoptionSummary />
+      </div>
+    </ApolloProvider>
   );
 }
