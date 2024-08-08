c7da5a9e93788b681f281c3b75624db84d771a67
Jeremy <jeremy@Admins-MacBook-Pro.local>
2024-08-09 00:19:33 +0200

    Embed more resources on the info page.

--- src/components/views/explore/faqs/ExploreFAQsView.jsx
+++ src/components/views/explore/faqs/ExploreFAQsView.jsx
@@ -1,27 +1,27 @@
 import AppSubSectionWrapper from "@/components/atoms/AppSubSectionWrapper/AppSubSectionWrapper";
 import EmbeddedIFrame from "@/components/atoms/EmbeddedIFrame/EmbeddedIFrame";
 
-const BCH_PODCAST_FAQ_URLS = "https://bitcoincashpodcast.com/faqs";
-const BCH_GURU_STATS_URL = "https://nfts.bch.guru/science";
-const TOKENAUT_URL = "https://tokenaut.cash/";
-const DEFI_LLAMA_URL = "https://defillama.com/chain/Bitcoincash"
+const BCH_PODCAST_FAQ_URL = "https://bitcoincashpodcast.com/faqs";
+const DISCOVER_CASH_URL = "https://discover.cash/"
+const BCHF_URL = "https://bitcoincashfoundation.org/";
+const HELPME_CASH_URL = "https://helpme.cash/";
 
 const SUBSECTIONS = [
   {
     name: "The Bitcoin Cash Podcast",
-    children: <EmbeddedIFrame src={BCH_PODCAST_FAQ_URLS} />
+    children: <EmbeddedIFrame src={BCH_PODCAST_FAQ_URL} />
   },
   {
-    name: "BCH Guru",
-    children: <EmbeddedIFrame src={BCH_GURU_STATS_URL} />
+    name: "Discover.cash",
+    children: <EmbeddedIFrame src={DISCOVER_CASH_URL} />
   },
   {
-    name: "Tokenaut",
-    children: <EmbeddedIFrame src={TOKENAUT_URL} />
+    name: "Bitcoin Cash Foundation",
+    children: <EmbeddedIFrame src={BCHF_URL} />
   },
   {
-    name: "DeFi Llama",
-    children: <EmbeddedIFrame src={DEFI_LLAMA_URL} />
+    name: "HelpMe.Cash",
+    children: <EmbeddedIFrame src={HELPME_CASH_URL} />
   }
 ]
 
