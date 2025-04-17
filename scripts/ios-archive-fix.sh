fdf3bf5ccc6f6637cb7855f06400535449aaa823
Kallisti <kallisti@kallisti.io>
2025-04-17 01:05:55 -0400

    script cleanup

--- scripts/ios-archive-fix.sh
+++ scripts/ios-archive-fix.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # user should execute from scripts dir
 CWD=$(pwd)
-cd ../ios/App/fastlane
+cd ios/App/fastlane
 sed -i.bak 's/source="$(readlink "${source}")"/source="$(readlink -f "${source}")"/' '../Pods/Target Support Files/Pods-App/Pods-App-frameworks.sh' && rm -f '../Pods/Target Support Files/Pods-App/Pods-App-frameworks.sh.bak'
 cd $CWD
