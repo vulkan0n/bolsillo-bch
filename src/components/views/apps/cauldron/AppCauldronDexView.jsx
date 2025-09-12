6ca934364cd25db858fff6cef027fd5f4cc3c0d2
Kallisti <kallisti@kallisti.io>
2025-09-12 17:46:33 -0400

    UI skeleton

--- src/components/views/apps/cauldron/AppCauldronDexView.jsx
+++ src/components/views/apps/cauldron/AppCauldronDexView.jsx
@@ -5,12 +5,14 @@ import {
   CloseOutlined,
   CopyOutlined,
   ExperimentOutlined,
+  TransactionOutlined,
 } from "@ant-design/icons";
 import { selectActiveWalletHash } from "@/redux/wallet";
 import FullColumn from "@/layout/FullColumn";
 import ViewHeader from "@/layout/ViewHeader";
 import Button from "@/atoms/Button";
-import Address from "@/atoms/Address";
+import Card from "@/atoms/Card";
+import { SatoshiInput } from "@/atoms/SatoshiInput";
 
 import { useClipboard } from "@/hooks/useClipboard";
 
@@ -29,7 +31,25 @@ export default function AppCauldronDexView() {
   return (
     <FullColumn>
       <ViewHeader title="Cauldron DEX" icon={ExperimentOutlined} />
-      <div className="p-1">
+      <div className="p-4">
+        <Card>
+          <div className="flex flex-col justify-center p-2">
+            <div>
+              <div className="text-neutral-500">You pay</div>
+              <SatoshiInput className="text-3xl my-1 p-2 rounded w-full border border-primary-700 shadow-inner" />
+            </div>
+            <div className="">
+              <Button icon={TransactionOutlined} padding="2" rounded="md" />
+            </div>
+            <div>
+              <div className="text-neutral-500">You receive</div>
+              <SatoshiInput className="text-3xl my-1 p-2 rounded w-full border border-primary-700 shadow-inner" />
+            </div>
+          </div>
+        </Card>
+        <div className="my-2">
+          <Button label="Swap" fullWidth rounded="lg" inverted labelSize="xl"/>
+        </div>
       </div>
     </FullColumn>
   );
